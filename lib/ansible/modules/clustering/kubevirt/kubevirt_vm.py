ed985ae2a5d9d18ead8ea7e96038bc633b19b931
Mariusz Mazur <mmazur@redhat.com>
2019-01-25 14:17:43 +0100

    python 3 fix, remove superfluous print

--- lib/ansible/modules/clustering/kubevirt/kubevirt_vm.py
+++ lib/ansible/modules/clustering/kubevirt/kubevirt_vm.py
@@ -199,7 +199,6 @@ import sys
 if hasattr(sys, '_called_from_test'):
     sys.path.append('lib/ansible/module_utils')
     import kubevirt
-    print kubevirt
     from kubevirt import (
         virtdict,
         VM_COMMON_ARG_SPEC,

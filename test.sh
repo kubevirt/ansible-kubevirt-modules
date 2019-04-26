13b05759f43bc63d12bad25cca5344b50558dde7
Mariusz Mazur <mmazur@redhat.com>
2019-04-26 13:13:48 +0200

    Use minishift with latest kubevirt; also gather debug info

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo "$(minikube ip) minikube" | sudo tee -a /etc/hosts
-
-## No HW virt
-kubectl create configmap -n kubevirt kubevirt-config --from-literal debug.useEmulation=true
-kubectl scale --replicas=0 deployment/virt-controller -n kubevirt
-kubectl scale --replicas=2 deployment/virt-controller -n kubevirt
+echo "$(minishift ip) minishift" | sudo tee -a /etc/hosts
 
 ## Prepare CDI
-export CDI_VERSION=v1.5.0
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-controller.yaml
+export CDI_VERSION=$(curl https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
+oc create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
+oc create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator-cr.yaml
 
 get_remaining_pods() {
-  kubectl get pods \
+  oc get pods \
     --all-namespaces \
     --field-selector=status.phase!=Running,status.phase!=Succeeded ;
 }
@@ -21,10 +17,22 @@ sleep 6;
 
 while [[ "$( get_remaining_pods 2>&1 | wc -l)" -gt 2 ]];
 do
-  kubectl get pods --all-namespaces
+  oc get pods --all-namespaces
   sleep 6;
 done
 
 ## Execute test
 ansible-playbook --version
 ansible-playbook -vvv tests/playbooks/all.yml
+RETVAL=$?
+
+## Gather debug info
+minishift status
+minishift logs
+minishift ssh 'ps auxw; docker ps -a; free -m; df -h'
+
+for i in pvcs vmis vms pods; do
+  oc get $i --all-namespaces
+done
+
+exit $RETVAL

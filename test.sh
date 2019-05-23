ba70c4ec3a2f624113243c8d763d90bb0d604808
Ondra Machacek <omachace@redhat.com>
2019-05-23 13:57:05 +0200

    Add app_deployment role

--- test.sh
+++ test.sh
@@ -2,14 +2,17 @@
 
 echo "$(minikube ip) minikube" | sudo tee -a /etc/hosts
 
+## Prepare CDI
+export VERSION=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator-cr.yaml
+
 ## No HW virt
 kubectl create configmap -n kubevirt kubevirt-config --from-literal debug.useEmulation=true --from-literal feature-gates=DataVolumes
 kubectl scale --replicas=0 deployment/virt-controller -n kubevirt
 kubectl scale --replicas=2 deployment/virt-controller -n kubevirt
-
-## Prepare CDI
-export CDI_VERSION=v1.5.0
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-controller.yaml
+kubectl scale --replicas=0 deployment/virt-api -n kubevirt
+kubectl scale --replicas=2 deployment/virt-api -n kubevirt
 
 get_remaining_pods() {
   kubectl get pods \
@@ -27,4 +30,5 @@ done
 
 ## Execute test
 ansible-playbook --version
+ansible-playbook -vvv tests/roles/deploy.yml
 ansible-playbook -vvv tests/playbooks/all.yml

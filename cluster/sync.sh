2aa50adccc6cf23f7f098233cc5c93d8148f907d
Mariusz Mazur <mmazur@redhat.com>
2019-05-27 11:28:55 +0000

    Switch Jenkins to openshift 3.11.0

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -4,13 +4,18 @@ set -xe
 
 source cluster/common.sh
 
+if [ $CLUSTER_TYPE == openshift ]; then
+  oc adm policy add-scc-to-user privileged -n kubevirt -z kubevirt-operator
+  oc adm policy add-scc-to-group anyuid system:authenticated
+fi
+
 KUBEVIRT_VER=$(curl -s https://github.com/kubevirt/kubevirt/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 CDI_VER=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-operator.yaml
-kubectl create configmap -n kubevirt kubevirt-config --from-literal feature-gates=DataVolumes
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-cr.yaml
+_kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-operator.yaml
+_kubectl create configmap -n kubevirt kubevirt-config --from-literal feature-gates=DataVolumes
+_kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-cr.yaml
 
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator.yaml
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator-cr.yaml
+_kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator.yaml
+_kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator-cr.yaml
 
-kubectl get ns
+_kubectl get ns

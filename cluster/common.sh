1fa9bc4aaee224a2fd5c5c86dee99cbc0378acd0
Mariusz Mazur <mmazur@redhat.com>
2019-05-30 16:06:13 +0200

    Switch Jenkins to openshift 3.11.0 (#236)

--- cluster/common.sh
+++ cluster/common.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 CLUSTER_PROVIDER=${CLUSTER_PROVIDER:-k8s-1.13.3}
-CLUSTER_NUM_NODES=${CLUSTER_NUM_NODES:-1}
+CLUSTER_NUM_NODES=${CLUSTER_NUM_NODES:-2}
 
 provider_prefix=${JOB_NAME:-${CLUSTER_PROVIDER}}${EXECUTOR_NUMBER}
 KUBE_CFG="$HOME/.kubeconfig-$provider_prefix"
 
+if [[ $CLUSTER_PROVIDER == os-* ]]; then
+  CLUSTER_TYPE=openshift
+  _kubectl() { oc "$@"; }
+elif [[ $CLUSTER_PROVIDER == k8s-* ]]; then
+  CLUSTER_TYPE=kubernetes
+  _kubectl() { kubectl "$@"; }
+fi
+
+oc() { cluster/oc "--kubeconfig=$KUBE_CFG" "$@"; }
 kubectl() { cluster/kubectl "--kubeconfig=$KUBE_CFG" "$@"; }
 gocli() { docker run --net=host --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock kubevirtci/gocli:latest --prefix "$provider_prefix" "$@"; }
 

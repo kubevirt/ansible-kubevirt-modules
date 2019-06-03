bfb491314efd16c1d9e811c80466a9d2181a14e5
Mariusz Mazur <mmazur@redhat.com>
2019-06-03 13:51:26 +0200

    Better logging + test multiple ansible versions (#243)
    * Run PVC tests first to maybe avoid hitting #240
    
    * Show useful info when finishing tests
    
    * Test both stable and devel ansible

--- automation/test.sh
+++ automation/test.sh
@@ -11,16 +11,35 @@ export PATH=$HOME/.local/bin:$PATH
 export K8S_AUTH_KUBECONFIG="$KUBE_CFG"
 
 # Make sure that the VM is properly shut down on exit
-trap '{ make cluster-down; }' EXIT SIGINT SIGTERM SIGSTOP
+trap '{ set +e; ansible --version; _kubectl get pvc -o yaml; _kubectl get vmis -o yaml; _kubectl get vms -o yaml; _kubectl get pods --all-namespaces; make cluster-down; }' EXIT SIGINT SIGTERM SIGSTOP
 
 make cluster-down
 make cluster-up
 make cluster-sync
 
+# Deps
 pip3 install --user pyasn1 pyopenssl openshift
-pip3 install --user git+https://github.com/ansible/ansible.git
 pip3 install --user -r requirements-dev.txt
+
+# STABLE ansible
+pip3 install --user ansible
 
 ansible-playbook --version
 ansible-playbook -vvv tests/playbooks/all.yml
 
+sleep 30
+
+# DEV ansible
+pip3 uninstall -y ansible
+pip3 install --user git+https://github.com/ansible/ansible.git
+
+# First make sure everything has been cleaned up
+objcount=$( for kind in vmi pvc vm vmirs; do _kubectl get $kind -o name; done | wc -l )
+if [ $objcount != 0 ]; then
+  echo "Playbooks did not clean up after themselves; aborting"
+  exit 1
+fi
+
+# And another one
+ansible-playbook --version
+ansible-playbook -vvv tests/playbooks/all.yml

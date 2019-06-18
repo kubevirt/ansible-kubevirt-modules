620b30a455f7c263187a3da59f1530629f5c566e
Mariusz Mazur <mmazur@redhat.com>
2019-06-18 14:02:06 +0000

    Jenkins: switch to bats for tests

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,39 +3,33 @@
 set -xe
 
 export CLUSTER_PROVIDER=$TARGET
-
 source cluster/common.sh
 
 # Ansible tweaks/settings
 export PATH=$HOME/.local/bin:$PATH
 export K8S_AUTH_KUBECONFIG="$KUBE_CFG"
 
+# Setup Python
 pip3 install --user virtualenv
 virtualenv py3env
 source py3env/bin/activate
 
-# Make sure that the VM is properly shut down on exit
+# Setup bats
+rm -rf bats-core bats
+git clone https://github.com/bats-core/bats-core.git
+mkdir bats
+( cd bats-core; git reset --hard 8789f910812afbf6b87dd371ee5ae30592f1423f; ./install.sh ../bats  )
+
+# Gather debug info and shut down the cluster on exit
 trap '{ set +e; ansible --version; _kubectl get pvc -o yaml; _kubectl get vmis -o yaml; _kubectl get vms -o yaml; _kubectl get pods --all-namespaces; make cluster-down; }' EXIT SIGINT SIGTERM SIGSTOP
 
+# Set up the cluster
 make cluster-down
 make cluster-up
 make cluster-sync
 
-for ANSIBLE_SOURCE in pip branch/stable-2.8 branch/devel; do
-  # Clean up and wait a bit
-  pip uninstall -y ansible || :
-  sleep 60
-
-  # First make sure everything has been cleaned up
-  objcount=$( for kind in vmi pvc vm vmirs vmipreset; do _kubectl get $kind -o name || :; done | wc -l )
-  if [ $objcount != 0 ]; then
-    echo "Playbooks did not clean up after themselves; aborting"
-    exit 1
-  fi
-
-  # Test
-  ci-common/install-ansible.sh $ANSIBLE_SOURCE
-
-  ansible-playbook --version
-  ansible-playbook -vvv tests/playbooks/all.yml
-done
+# Run the tests
+echo -e "########################################################" \
+      "\n################### RUNNING TESTS ######################" \
+      "\n########################################################"
+bats/bin/bats jenkins/tests.bats
---
+++ jenkins/tests.bats
@@ -0,0 +1,32 @@
+
+run_test() {
+  # Clean up and wait a bit
+  pip uninstall -y ansible || :
+  sleep 60
+
+  # First make sure everything has been cleaned up
+  objcount=$( for kind in vmi pvc vm vmirs vmipreset; do kubectl get $kind -o name || :; done | wc -l )
+  if [ $objcount != 0 ]; then
+    echo "Playbooks did not clean up after themselves; aborting"
+    false
+  fi
+
+  # Test
+  ci-common/install-ansible.sh $1
+
+  ansible-playbook --version
+  ansible-playbook -vvv tests/playbooks/all.yml
+}
+
+@test "pip" {
+  run_test pip
+}
+
+@test "branch/stable-2.8" {
+  run_test branch/stable-2.8
+}
+
+@test "branch/devel" {
+  run_test branch/devel
+}
+

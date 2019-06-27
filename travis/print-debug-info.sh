307d80b889933fcb8a0a873b467d8cfcb87e0a0e
Mariusz Mazur <mmazur@redhat.com>
2019-06-27 11:54:13 +0200

    Workaround for travis+minikube instability (#262)
    * Travis: bump debug output on error
    
    * Travis/minikube: sleep 90 seconds between playbooks

--- travis/print-debug-info.sh
+++ travis/print-debug-info.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -x
+
+kubectl get pods --all-namespaces
+
+free -m
+docker ps
+docker ps -q|xargs docker stats --no-stream
+
 kubectl get configmap -n kubevirt kubevirt-config -o yaml
 kubectl get pvc -o yaml
 kubectl get vmis -o yaml
 kubectl get vms -o yaml
-kubectl get pods --all-namespaces
 
+kubectl describe nodes
+
+minikube logs

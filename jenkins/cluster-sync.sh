5ba5af18131e9a679a14765260cccf4ef5777903
Mariusz Mazur <mmazur@redhat.com>
2019-07-01 11:54:04 +0200

    Jenkins: switch to openshift 3 (#264)
    * Complete template playbook with openshift autodetection
    
    * Jenkins: switch to os-3.11.0
    
    * OC3: skip pvc as there are issues with CDI

--- jenkins/cluster-sync.sh
+++ jenkins/cluster-sync.sh
@@ -2,26 +2,29 @@
 
 set -xe
 
+cluster-up/oc.sh adm policy add-scc-to-user privileged -n kubevirt -z kubevirt-operator
+cluster-up/oc.sh adm policy add-scc-to-group anyuid system:authenticated
+
 KUBEVIRT_VER=$(curl -s https://github.com/kubevirt/kubevirt/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 CDI_VER=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
-cluster-up/kubectl.sh apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator.yaml
-cluster-up/kubectl.sh apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator-cr.yaml
+cluster-up/oc.sh apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator.yaml
+cluster-up/oc.sh apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VER/cdi-operator-cr.yaml
 
-cluster-up/kubectl.sh apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-operator.yaml
-cluster-up/kubectl.sh create configmap -n kubevirt kubevirt-config --from-literal feature-gates=DataVolumes
-cluster-up/kubectl.sh apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-cr.yaml
+cluster-up/oc.sh apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-operator.yaml
+cluster-up/oc.sh create configmap -n kubevirt kubevirt-config --from-literal feature-gates=DataVolumes
+cluster-up/oc.sh apply -f https://github.com/kubevirt/kubevirt/releases/download/$KUBEVIRT_VER/kubevirt-cr.yaml
 
 
 (
-  cluster-up/kubectl.sh wait --timeout=240s --for=condition=Ready -n kubevirt kv/kubevirt ;
+  cluster-up/oc.sh wait --timeout=240s --for=condition=Ready -n kubevirt kv/kubevirt ;
 ) || {
   echo "Something went wrong"
-  cluster-up/kubectl.sh describe -n kubevirt kv/kubevirt
-  cluster-up/kubectl.sh describe pods -n kubevirt
+  cluster-up/oc.sh describe -n kubevirt kv/kubevirt
+  cluster-up/oc.sh describe pods -n kubevirt
   exit 1
 }
 
 sleep 12
 
-cluster-up/kubectl.sh describe nodes
+cluster-up/oc.sh describe nodes
 
